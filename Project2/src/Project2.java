7ec3fc27dbd0d9b6533a2f22c7a2d8283b8425b5
eglenn <eglenn@localhost>
2022-11-05 19:46:36 -0400

    4th

---
+++ Project2/src/Project2.java
@@ -0,0 +1,24 @@
+/* Name: Ethan Glenn
+* File Name: Project2.java
+* Date: 4 November, 2022
+* Program Description: Executes maze games. */
+import java.util.Scanner;
+
+public class Project2 {
+
+	public static void main(String[] args) throws Exception { // java Project2 maze.txt, Run Config -> maze.txt
+		Scanner input = new Scanner(System.in);
+		DragonMaze game = new DragonMaze();
+		String mazeFile = "";
+		if (args.length > 0) {
+			;
+		} else {
+			System.out.println("No command line argument entered. Please enter the name of the maze file: ");
+			mazeFile = input.nextLine();
+		}
+		System.out.println("Reading file: " + mazeFile);
+		game.loadMazeFile(mazeFile);
+		game.printMaze();
+	}
+
+}
